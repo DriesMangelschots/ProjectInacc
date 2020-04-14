3c752fabe74156e650ae6b4b1ae8337424f904ce
DriesMangelschots <drieskeboy@gmail.com>
2020-04-14 22:47:51 +0200

    Onderlijnen navbar, buttons & lightbox

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,4 @@
-// Regular map
-function regular_map() {
-    var locatie = {lat: 51.2198468, lng: -4.9107236};
-
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: locatie
-        
-    });
-
-    var aanduiding = new google.maps.Marker({
-        position: locatie,
-        map: map,
-        title: "Inacc"
-    });
-}
-
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
